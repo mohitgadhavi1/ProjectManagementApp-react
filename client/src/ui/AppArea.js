6d16729f1e9d979abe6ce1ada5b31f9477b04fdf
Mohit <mohitgadhavi1@gmail.com>
2022-02-03 12:18:13 +0530

    reg

--- client/src/ui/AppArea.js
+++ client/src/ui/AppArea.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Task from "../components/TaskApp/Task";
-import Google from "../components/Google";
+import Project from "../components/Project";
 
 export default function AppArea() {
   return (
     <div className="overflow-auto  h-screen pb-24 pt-2 pr-2 pl-3 md:pt-0 md:pr-3 md:pl-0">
       <div className="flex flex-col flex-wrap sm:flex-row ">
         <div className="w-full sm:w-1/2 xl:w-1/3">
-          <Google />
+          <Project />
         </div>
         <div className="w-full sm:w-1/2 xl:w-1/3">
           <Task />
